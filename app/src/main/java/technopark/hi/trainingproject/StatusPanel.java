1c18c78069a43ed0b351d2a9c8a3d07980c3d75f
kakha <kakha@soft.ge>
2016-11-28 17:17:51 +0400

    statusPanel Custom View

---
+++ app/src/main/java/technopark/hi/trainingproject/StatusPanel.java
@@ -0,0 +1,30 @@
+package technopark.hi.trainingproject;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by kakha on 11/28/16.
+ */
+
+public class StatusPanel extends TextView {
+
+    public StatusPanel(Context context) {
+        super(context);
+    }
+
+    public StatusPanel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        //TypedArray typedArray=context.obtainStyledAttributes(attrs, R.styleable.StatusPanel);
+        //typedArray.getString(0);
+        setStatus(context);
+    }
+
+    private void setStatus(Context context){
+        setTextColor(ContextCompat.getColor(context, R.color.colorWhite));
+        setText("New");
+    }
+}
